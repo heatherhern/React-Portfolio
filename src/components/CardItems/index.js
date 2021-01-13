13cf75107f8f485774aaf8b1f6f4e4741d5fb95b
Heather Hern <heathernewlin1@gmail.com>
2021-01-13 17:29:32 -0500

    Added additional cards

--- src/components/CardItems/index.js
+++ src/components/CardItems/index.js
@@ -1,22 +1,20 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./style.css";
 
 function CardItem(props) {
     return (
         <>
-            <li className="cards_item">
-                <Link className="cards_item_link" to={props.path}>
-                    <figure className='cards_item_pic_wrap' data-category={props.label}>
-                        <img 
-                        src={props.src} 
-                        alt="portfolio"
-                        className='cards_item_img' 
+            <li className='cards__item'>
+                <Link className='cards__item__link' to={props.path}>
+                    <figure className='cards__item__pic-wrap' data-category={props.label}>
+                        <img
+                            className='cards__item__img'
+                            alt='Travel Image'
+                            src={props.src}
                         />
                     </figure>
-                    <div className='cards_item_info'>
-                        <h5 className='cards_item_text'>{props.text}
-                        </h5>
+                    <div className='cards__item__info'>
+                        <h5 className='cards__item__text'>{props.text}</h5>
                     </div>
                 </Link>
             </li>
--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -1,25 +1,51 @@
-import React from 'react'
-import CardItem from '../CardItems'
-import "./style.css";
+import React from 'react';
+import './style.css';
+import CardItem from '../CardItems';
 
 function Cards() {
     return (
         <div className='cards'>
-            <h1>this is a title thing </h1>
-            <div className='cards_container'>
-                <div className='cards_wrapper'>
-                    <ul className='cards_items'>
-                        <CardItem 
-                        src='../../images/warehouse.jpg'
-                        text="this is som eshitty text from line 14 of cards index js"
-                        label="idfk a portfolio category"
-                        path="/something"
+            <h1>Check out these EPIC Destinations!</h1>
+            <div className='cards__container'>
+                <div className='cards__wrapper'>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src="../.././images/desk.jpg"
+                            text='Explore the hidden waterfall deep inside the Amazon Jungle'
+                            label='Adventure'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-2.jpg'
+                            text='Travel through the Islands of Bali in a Private Cruise'
+                            label='Luxury'
+                            path='/services'
+                        />
+                    </ul>
+                    <ul className='cards__items'>
+                        <CardItem
+                            src='images/img-3.jpg'
+                            text='Set Sail in the Atlantic Ocean visiting Uncharted Waters'
+                            label='Mystery'
+                            path='/services'
+                        />
+                        <CardItem
+                            src='images/img-4.jpg'
+                            text='Experience Football on Top of the Himilayan Mountains'
+                            label='Adventure'
+                            path='/products'
+                        />
+                        <CardItem
+                            src='images/img-8.jpg'
+                            text='Ride through the Sahara Desert on a guided camel tour'
+                            label='Adrenaline'
+                            path='/sign-up'
                         />
                     </ul>
                 </div>
             </div>
         </div>
-    )
+    );
 }
 
-export default Cards
+export default Cards;
