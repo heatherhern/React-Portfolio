c9b45cf68441cd6328448c51d9134f510358c054
Heather Hern <heathernewlin1@gmail.com>
2021-01-19 16:29:59 -0500

    Added resume download

--- src/components/pages/Contactpage.js
+++ src/components/pages/Contactpage.js
@@ -3,6 +3,7 @@ import ListGroup from 'react-bootstrap/ListGroup';
 import Hero from "../Hero";
 import Footer from "../Footer";
 import warehouse from "../../images/warehouse.jpg";
+import resumePDF from '../../Assets/HeatherHernResume2021.pdf';
 import "../../App.css";
 
 
@@ -16,11 +17,10 @@ function Contactpage() {
                 message="let's work together!"                
             />
             <ListGroup className="list-group-contact" horizontal>
-                {/* <row > */}
                         <ListGroup.Item className="list-group-item" variant="secondary">heathernewlin1@gmail.com</ListGroup.Item>
                         <ListGroup.Item className="list-group-item" variant="secondary">615.477.7223</ListGroup.Item>
                         <ListGroup.Item className="list-group-item" action href="/github" variant="secondary"><i class="fab fa-github"></i> @heatherhern</ListGroup.Item>
-                {/* </row> */}
+                        <ListGroup.Item className="list-group-item" action href={resumePDF} download="HeatherHernResume2021.pdf" target="_blank" rel="noopener noreferrer" variant="secondary"><i class="fas fa-download"></i> Resume</ListGroup.Item>
                     </ListGroup>
             <Footer />
         </>
